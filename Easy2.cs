5baed20a1d808423fbbfa6688f5334794a3c879d
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2025-03-04 20:33:05 +0200

    added TwoSum method

--- Easy2.cs
+++ Easy2.cs
@@ -1,6 +1,11 @@
 ﻿using System;
-
+using System.Collections;
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You May assume that each input would have exactly one solution, and you may not use the same element twice.
 public class Easy2
 {
-
+    public ArrayList<int> TwoSum(int nums[])
+    {
+        
+    }
 }
