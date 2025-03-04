7ef1f8a6817d9fcf714c1234b3b086a087db49c5
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2025-03-04 18:36:49 +0200

    Add project files.

---
+++ Easy1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+public class Easy1
+{
+    public static Boolean CheckAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        else
+        {
+            int cnt = 0;
+            foreach (char cT in t)
+            {
+                if (s.Contains(cT))
+                {
+                    cnt++;
+                }
+            }
+            if (cnt == s.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Easy1:");
+Console.WriteLine(Easy1.CheckAnagram("mug", "gum"));
+Console.WriteLine(Easy1.CheckAnagram("mug", "rug"));
+Console.WriteLine(Easy1.CheckAnagram("mug", "Jeff"));
