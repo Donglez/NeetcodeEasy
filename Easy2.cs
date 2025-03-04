51f7b259258b76da3336a5806935147c895d9fd2
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2025-03-04 21:10:51 +0200

    Easy2.cs now solves solution correctly

--- Easy2.cs
+++ Easy2.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 //You May assume that each input would have exactly one solution, and you may not use the same element twice.
 public class Easy2
 {
-    public ArrayList<int> TwoSum(int nums[])
+    public static string TwoSum(int[] nums, int target)
     {
-        
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            map.Add(nums[i], i);
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (map.Keys.Contains(diff) && map[diff] != map[nums[i]])
+            {
+                return "[" + map[nums[i]] + ", " + map[diff] + "]";
+            }
+        }
+        return "";
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine(Easy1.CheckAnagram("mug", "gum"));
 Console.WriteLine(Easy1.CheckAnagram("mug", "rug"));
 Console.WriteLine(Easy1.CheckAnagram("mug", "Jeff"));
+
+Console.WriteLine("Easy2:");
+Console.WriteLine(Easy2.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+Console.WriteLine(Easy2.TwoSum(new int[] { 2, 1, 5, 3 }, 4));
