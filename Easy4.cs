0b11b70dd842ddfbf9f2fec5634262fc402b4f4b
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2025-03-05 18:40:00 +0200

    Implemented Easy4 using a dictionary

---
+++ Easy4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//Two Sum II.
+//Given an array of integers that is already sorted in ascending order, find two numbers such that they add
+//up to a specific target number.
+//The function twoSum should return indices of the two numbers such that they add up to the target,
+//where index 1 is less than index 2.
+
+//Your returned answers (both indeces 1 and 2) are not zero based
+//You may assume that each input would have exactly one solution and you may not use the same element twice.
+public class Easy4
+{
+	public static string TwoSum(int[] nums, int target)
+	{
+		Dictionary<int, int> diffs = new Dictionary<int, int>();
+		string output = "";
+		for (int i = 0; i < nums.Length; i++)
+		{
+			diffs.Add(target - nums[i], i + 1);
+		}
+		for (int i = 0; i < nums.Length; i++)
+		{
+			if (diffs.ContainsKey(nums[i]) && diffs[nums[i]] != (i+1))
+			{
+				output = "[" + diffs[nums[i]] + ", " + (i + 1) + "]";
+			}
+		}
+		return output;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -10,3 +10,7 @@ Console.WriteLine(Easy2.TwoSum(new int[] { 2, 1, 5, 3 }, 4));
 Console.WriteLine("Easy3:");
 Console.WriteLine(Easy3.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
 Console.WriteLine(Easy3.MaxSubArray(new int[] { 2, -1, 5, 3 , -7, 2, -1, 6, 20, -100}));
+
+Console.WriteLine("Easy4:");
+Console.WriteLine(Easy4.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
+Console.WriteLine(Easy4.TwoSum(new int[] { 1, 3, 4, 5, 7, 10, 11 }, 9));
