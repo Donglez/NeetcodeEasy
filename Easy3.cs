80fa71a3a2351fd0b0338aadada812c96f2c304b
ArcaneWinter <deanwilliams.arcanewinter@gmail.com>
2025-03-05 18:10:46 +0200

    Implemented Easy3 solution

---
+++ Easy3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Numerics;
+
+//Given an integer array (nums), find the contiguous subarray (containing at least one number)
+//which has the has the largest sum and return its sum
+public class Easy3
+{
+	public static int MaxSubArray(int[] nums)
+	{
+		
+		int sum = nums[0];
+        int maxSum = sum;
+
+        for (int i = 1; i < nums.Length; i++)
+		{
+			if (nums[i] + sum >= nums[i])
+			{
+				sum += nums[i];
+			}
+			else
+			{
+				sum = nums[i];
+			}
+			maxSum = Math.Max(sum, maxSum);
+		}
+		return maxSum;
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,3 +6,7 @@ Console.WriteLine(Easy1.CheckAnagram("mug", "Jeff"));
 Console.WriteLine("Easy2:");
 Console.WriteLine(Easy2.TwoSum(new int[] { 2, 7, 11, 15 }, 9));
 Console.WriteLine(Easy2.TwoSum(new int[] { 2, 1, 5, 3 }, 4));
+
+Console.WriteLine("Easy3:");
+Console.WriteLine(Easy3.MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+Console.WriteLine(Easy3.MaxSubArray(new int[] { 2, -1, 5, 3 , -7, 2, -1, 6, 20, -100}));
